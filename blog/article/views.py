dafd659d2290486eb9ecd16a7d1154ea1c07cdf7
Alexander <satyugov@mail.ru>
2023-03-15 20:00:35 +0400

    Lesson_2.1

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,49 +1,114 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
 
+from blog.user.views import get_user_name
 
-from blog.users.views import get_user_name
-
-article = Blueprint('article', __name__, url_prefix='/article', static_folder='../static')
+article = Blueprint("article", __name__, url_prefix="/article", static_folder="../static")
 
 ARTICLES = {
-    1: {'name': 'Age of Empires',
-        'text': 'text_text',
-        'author': 2
-        },
-
-
-    2: {'name': 'Myst',
-        'text': 'text_text_text ',
-        'author': 3
+    1: {
+        "title": "Wonderful Kindom",
+        "text": "Once upon a time",
+        "author": 2
+    },
+    2: {
+        "title": "Time for dream",
+        "text": "In fantastic world",
+        "author": 3
+    },
+    3: {
+        "title": "Sun in garden",
+        "text": "More times ago",
+        "author": 1
     },
 
-    3:{
-        'name': 'Dayz world',
-        'text': 'textZ',
-        'author': 1
-    }
-
 }
-@article.route('/')
+
+
+@article.route("/")
 def article_list():
     return render_template(
-        'articles/list.html',
-        articles = ARTICLES
+        "articles/list.html",
+        articles=ARTICLES
     )
 
 
-@article.route('/<int:pk>')
+@article.route("/<int:pk>")
 def get_article(pk: int):
     if pk in ARTICLES:
         article_raw = ARTICLES[pk]
     else:
-        raise NotFound(f'Article id {pk}, not found')
-    name = article_raw['name']
-    text = article_raw['text']
-    author = get_user_name(article_raw['author'])
+        raise NotFound("Article id:{}, not found".format(pk))
+    title = article_raw["title"]
+    text = article_raw["text"]
+    author = get_user_name(article_raw["author"])
     return render_template(
-        'articles/details.html',
-        name=name,
+        "articles/details.html",
+        title=title,
+        text=text,
         author=author
     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Blueprint, render_template
+# from werkzeug.exceptions import NotFound
+#
+#
+# from blog.user.views import get_user_name
+#
+# article = Blueprint('article', __name__, url_prefix='/article', static_folder='../static')
+#
+# ARTICLES = {
+#     1: {'name': 'Age of Empires',
+#         'text': 'text_text',
+#         'author': 2
+#         },
+#
+#
+#     2: {'name': 'Myst',
+#         'text': 'text_text_text ',
+#         'author': 3
+#     },
+#
+#     3:{
+#         'name': 'Dayz world',
+#         'text': 'textZ',
+#         'author': 1
+#     }
+#
+# }
+# @article.route('/')
+# def article_list():
+#     return render_template(
+#         'articles/list.html',
+#         articles = ARTICLES
+#     )
+#
+#
+# @article.route('/<int:pk>')
+# def get_article(pk: int):
+#     if pk in ARTICLES:
+#         article_raw = ARTICLES[pk]
+#     else:
+#         raise NotFound(f'Article id {pk}, not found')
+#     name = article_raw['name']
+#     text = article_raw['text']
+#     author = get_user_name(article_raw['author'])
+#     return render_template(
+#         'articles/details.html',
+#         name=name,
+#         author=author
+#     )
--- blog/report/views.py
+++ blog/report/views.py
@@ -1,10 +1,33 @@
 from flask import Blueprint, render_template
 
-report = Blueprint('report', __name__, url_prefix='reports', static_folder='../static')
+report = Blueprint("report", __name__, url_prefix="/reports", static_folder="../static")
 
-@report.route('/')
+
+@report.route("/")
 def report_list():
     return render_template(
-        'reports/list.html',
-        reports = [1, 2, 3, 4, 5]
+        "base.html",
+        reports=[1,231,4]
     )
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Blueprint, render_template
+#
+# report = Blueprint('report', __name__, url_prefix='reports', static_folder='../static')
+#
+# @report.route('/')
+# def report_list():
+#     return render_template(
+#         'reports/list.html',
+#         reports = [1, 2, 3, 4, 5]
+#     )
